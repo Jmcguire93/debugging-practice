82083f6b83e93bacdf9dd57a6c105b2beecce5f5
James <james.ryan.mcguire@gmail.com>
2021-06-09 17:06:39 -0700

    Fixes commented code and spacing

--- exercise.rb
+++ exercise.rb
@@ -1,66 +1,66 @@
 #Exercise 1: Print a message to a pirate.
 
-# puts "Hello!"
-# greeting = gets.chomp()
-# answer = greeting
+puts "Hello!"
+greeting = gets.chomp()
+answer = greeting
 
-# if answer["Arrr!"]
-#   puts "Go away, pirate."
-# else
-#   puts "Greetings, hater of pirates!"
-# end
+if answer["Arrr!"]
+  puts "Go away, pirate."
+else
+  puts "Greetings, hater of pirates!"
+end
+
 
 #Exercise 2: Write a function to print when each person died in a sentence.
 
-# dickens = ["Charles Dickens", 1870]
-# thackeray = ["William Thackeray", 1863]
-# trollope = ["Anthony Trollope", 1882]
-# hopkins = ["Gerard Manley Hopkins", 1889]
+dickens = ["Charles Dickens", 1870]
+thackeray = ["William Thackeray", 1863]
+trollope = ["Anthony Trollope", 1882]
+hopkins = ["Gerard Manley Hopkins", 1889]
 
-# def died(array)
-#   name = array[0]
-#   year = array[1]
-#   puts  "#{name} died in #{year}."
-# end
+def died(array)
+  name = array[0]
+  year = array[1]
+  puts  "#{name} died in #{year}."
+end
 
-# puts died(dickens)
-# puts died(thackeray)
-# puts died(trollope)
-# puts died(hopkins)
+puts died(dickens)
+puts died(thackeray)
+puts died(trollope)
+puts died(hopkins)
 
 
 #Exercise 3: Print a message to a time traveler.
 
-# puts "Greetings! What is your year of origin?"
-# origin = gets.chomp.to_i
-# if origin < 1900
-#   puts "That's the past!"
-# elsif origin > 1900 && origin < 2020
-#   puts "That's the present!"
-# else
-#   puts "That's the future!"
-# end
+puts "Greetings! What is your year of origin?"
+origin = gets.chomp.to_i
+if origin < 1900
+  puts "That's the past!"
+elsif origin > 1900 && origin < 2020
+  puts "That's the present!"
+else
+  puts "That's the future!"
+end
 
 
 #Exercise 4: Make a class to store and display a person’s info.
 
-
-# class Person
-#   attr_reader :first_name, :last_name
+class Person
+  attr_reader :first_name, :last_name
   
-#   def initialize(first_name, last_name)
-#     @first_name = first_name
-#     @last_name = last_name
-#   end
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
 
-#   def full_name
-#     puts "#{first_name} #{last_name}"
-#   end
+  def full_name
+    puts "#{first_name} #{last_name}"
+  end
+end
 
-# end
-# person1 = Person.new("Thomas", "Jefferson")
-# p person1.first_name
-# person1.full_name
+person1 = Person.new("Thomas", "Jefferson")
+p person1.first_name
+person1.full_name
 
 
 #Exercise 5: Write a program that will average 3 numeric exam grades, return an average test score, 
